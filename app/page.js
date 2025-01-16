5027b784548717bc2a858225dd5d3dde9bf781d6
Türkalp Kucur <turkalp.kucur@gmail.com>
2025-01-16 17:03:43 +0300

    firstcommit
    firstcommit

--- app/page.js
+++ app/page.js
@@ -1,15 +1,9 @@
-
-
-import Link from 'next/link'
 export default function Home() {
-
   return (
-    < main>
-
-      <p><Link href="/meals">MEALS..</Link></p>
-      <p><Link href="/meals/share">SHARE..</Link></p>
-      <p><Link href="/community">COMMUNİTY..</Link></p>
-
+    <main>
+      <h1 style={{ color: 'white', textAlign: 'center' }}>
+        Time to get started!
+      </h1>
     </main>
   );
 }
