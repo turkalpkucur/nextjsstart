75c6ccf89fbce0043bc0974a019c1b660a66b268
Türkalp Kucur <turkalp.kucur@gmail.com>
2025-01-17 16:09:06 +0300

    güncelleme
    güncelleme

---
+++ app/main-header.js
@@ -0,0 +1,27 @@
+import Link from 'next/link'
+import logoImg from '@/assets/logo.png';
+import classes from './main-header.module.css';
+export default function MainHeader() {
+    return (
+        <header className={classes.header}>
+            <Link className={classes.logo} href="/">
+                <img src={logoImg.src} alt="a plate with food in it" />
+                NextLevel Food
+            </Link>
+            <nav className={classes.nav}>
+                <ul>
+                    <li>
+                        <Link href="/meals">Meals</Link>
+                    </li>
+                    <li>
+                        <Link href="/community">Foodies Community</Link>
+                    </li>
+                    <li>
+                        <Link href="/meals/share">Share Meal</Link>
+                    </li>
+                </ul>
+            </nav>
+        </header>
+
+    )
+}
