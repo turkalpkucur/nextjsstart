b4ade844b8f8db20ab05aa0264ecf082741876fb
Türkalp Kucur <turkalp.kucur@gmail.com>
2025-01-20 09:00:49 +0300

    fix
    fix

--- app/components/main-header/nav-link.js
+++ app/components/main-header/nav-link.js
@@ -5,7 +5,7 @@ import classes from './nav-link.module.css';
 export default function NavLink({ href, children }) {
     const path = usePathname();
     return (
-        <Link href={href} className={path.startsWith(href) ? `${classes.link}` `${classes.active}` : classes.link}>
+        <Link href={href} className={path.startsWith(href) ? `${classes.link} ${classes.active}` : classes.link}>
             {children}
         </Link>
     )
